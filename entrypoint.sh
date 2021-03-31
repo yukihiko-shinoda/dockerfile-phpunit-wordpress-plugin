978308656e737fe977f7dc8795b2fafcf7f85275
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2021-03-31 19:46:52 +0900

    Support MySQL 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ DATABASE_HOST=${DATABASE_HOST-database}
 DATABASE_PORT=${DATABASE_PORT-3306}
 WORDPRESS_VERSION=${WORDPRESS_VERSION-latest}
 
-wait-for-it --timeout=30 "${DATABASE_HOST}:${DATABASE_PORT}" -- install-wp-tests "${DATABASE_NAME}" "${DATABASE_USER}" "${DATABASE_PASSWORD}" "${DATABASE_HOST}:${DATABASE_PORT}" "${WORDPRESS_VERSION}"
+# Sometimes, timeout occurred when set 45 seconds for MySQL 8
+wait-for-it --timeout=60 "${DATABASE_HOST}:${DATABASE_PORT}" -- install-wp-tests "${DATABASE_NAME}" "${DATABASE_USER}" "${DATABASE_PASSWORD}" "${DATABASE_HOST}:${DATABASE_PORT}" "${WORDPRESS_VERSION}"
 exec "${@}"
