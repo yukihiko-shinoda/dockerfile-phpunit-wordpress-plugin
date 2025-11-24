1020fc9247310b52697da605942cd2f321138839
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2025-11-24 17:47:19 +0900

    Refactor for simplifying

--- shell-scripts-to-copy/entrypoint.sh
+++ shell-scripts-to-copy/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+DOMAIN_NAME=${DOMAIN_NAME-database}
+DATABASE_NAME=${DATABASE_NAME-wordpress}
+DATABASE_USER=${DATABASE_USER-root}
+DATABASE_PASSWORD=${DATABASE_PASSWORD}
+DATABASE_HOST=${DATABASE_HOST-database}
+DATABASE_PORT=${DATABASE_PORT-3306}
+WORDPRESS_VERSION=${WORDPRESS_VERSION-latest}
+
 SERVERCERT="/etc/pki/tls/certs/servercert-${DOMAIN_NAME}.pem"
 SERVERKEY="/etc/pki/tls/private/serverkey-${DOMAIN_NAME}.pem"
 while :; do
@@ -12,13 +20,6 @@ done
 
 cp -p /etc/pki/CA/cacert-${DOMAIN_NAME}.pem /usr/local/share/ca-certificates/cacert-${DOMAIN_NAME}.crt
 update-ca-certificates
-
-DATABASE_NAME=${DATABASE_NAME-wordpress}
-DATABASE_USER=${DATABASE_USER-root}
-DATABASE_PASSWORD=${DATABASE_PASSWORD}
-DATABASE_HOST=${DATABASE_HOST-database}
-DATABASE_PORT=${DATABASE_PORT-3306}
-WORDPRESS_VERSION=${WORDPRESS_VERSION-latest}
 
 # Sometimes, timeout occurred when set 45 seconds for MySQL 8
 wait-for-it --timeout=90 "${DATABASE_HOST}:${DATABASE_PORT}" -- install-wp-tests "${DATABASE_NAME}" "${DATABASE_USER}" "${DATABASE_PASSWORD}" "${DATABASE_HOST}:${DATABASE_PORT}" "${WORDPRESS_VERSION}"
